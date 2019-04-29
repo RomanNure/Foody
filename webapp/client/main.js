4bdd7930b8c5635986a750728c1be06e25c65b1d
Roman <rk@pangnote.com>
2019-04-30 01:37:39 +0300

    add react router and rebild webapp

--- webapp/client/main.js
+++ webapp/client/main.js
@@ -1,1 +1,8 @@
-console.log(`Greetings from ${module.id}!`);
+import React from 'react';
+import { render } from 'react-dom';
+import Root from '../imports/ui/router.js';
+Meteor.startup(() => {
+  render(<Root/>, document.getElementById('root'));
+});
+
+
