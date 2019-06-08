f1d6ea6a4ddd5012880076f0041848a920803d79
Roman <rk@pangnote.com>
2019-06-08 23:15:19 +0300

    -update main

--- webapp/imports/ui/page/main.js
+++ webapp/imports/ui/page/main.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { Dropdown, Button } from 'react-materialize'
+import ReactDOM from 'react-dom';
 
 export default class Main extends Component {
 	constructor(props){
@@ -11,30 +13,6 @@ export default class Main extends Component {
 			},
 
 		}
-	}
-	componentDidMount(){
-		document.addEventListener('DOMContentLoaded', function() {
-			var elems = document.querySelectorAll('.carousel');
-			//	var instances = M.Carousel.init(elems, options);
-
-			var instance = M.Carousel.init({
-				fullWidth: true
-			});
-		});
-		document.addEventListener('DOMContentLoaded', function() {
-			var elems = document.querySelectorAll('.dropdown-trigger');
-			var instances = M.Dropdown.init(elems, options);
-		  });
-	}
-	drop = () => (e) =>{
-		e.preventDefault()
-	 var instance = Materialize.Dropdown.getInstance(elem);
-
-	}
-	dropdown = () => (e) => {
-		e.preventDefault()
-		console.log('ok',e);
-		e.target.parentElement.classList.toggle('open');
 	}
 	render() {
 		console.log('header');
@@ -88,10 +66,11 @@ export default class Main extends Component {
 								}
 							</div>
 							<div className="row">
-								<div className="nav-item nav-dropdown col-9">
-									<a onClick={this.dropdown()} className="nav-link nav-dropdown-toggle"><i className="material-icons right">arrow_drop_down</i></a>
+								<Dropdown trigger={
+									<Button>Drop me!</Button>
+								}>
 									<ul className="nav-dropdown-items">      
-										{/*	<li className="side">
+											<li className="side">
 										<NavLink to="/account" className="nav-link waves-effect waves-light"><i className="small material-icons prefix">account_box</i> Account</NavLink>  
 									</li>
 									<li className="side">
@@ -107,9 +86,30 @@ export default class Main extends Component {
 									<li className="side">
 										<NavLink to="/massages" className="nav-link waves-effect waves-light"><i className="small material-icons prefix">mail_outline</i> Massages</NavLink>  
 									</li>
-									*/}
+								</ul>
+							</Dropdown>;
+							{/*					<div className="nav-item nav-dropdown col-9">
+									<a  className="nav-link nav-dropdown-toggle"><i className="material-icons right">arrow_drop_down</i></a>
+									<ul className="nav-dropdown-items">      
+											<li className="side">
+										<NavLink to="/account" className="nav-link waves-effect waves-light"><i className="small material-icons prefix">account_box</i> Account</NavLink>  
+									</li>
+									<li className="side">
+										<NavLink to="/settings" className="nav-link waves-effect waves-light"><i className="small material-icons prefix">settings</i> Settings</NavLink>  
+									</li>
+									<li className="side">
+										<NavLink to="/notification" className="nav-link waves-effect waves-light"><i className="small material-icons prefix">notifications</i> Notifications</NavLink>  
+									</li>
+									<li className="side">
+										<NavLink to="/favourite" className="nav-link waves-effect waves-light"><i className="small material-icons prefix">favorite_border</i> Favourite</NavLink>  
+									</li>
+									<li className="divider"></li>
+									<li className="side">
+										<NavLink to="/massages" className="nav-link waves-effect waves-light"><i className="small material-icons prefix">mail_outline</i> Massages</NavLink>  
+									</li>
 								</ul>
 							</div>
+							*/}						
 						</div>
 					</div>
 				</div>
@@ -125,33 +125,17 @@ export default class Main extends Component {
 			</div>
 
 
-			<div class="fixed-action-btn">
-				<a class="btn-floating btn-large red">
-					<i class="large material-icons">mode_edit</i>
+			<div className="fixed-action-btn">
+				<a className="btn-floating btn-large red">
+					<i className="large material-icons">mode_edit</i>
 				</a>
 				<ul>
-					<li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-					<li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-					<li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-					<li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+					<li><a className="btn-floating red"><i className="material-icons">insert_chart</i></a></li>
+					<li><a className="btn-floating yellow darken-1"><i className="material-icons">format_quote</i></a></li>
+					<li><a className="btn-floating green"><i className="material-icons">publish</i></a></li>
+					<li><a className="btn-floating blue"><i className="material-icons">attach_file</i></a></li>
 				</ul>
 			</div>
-			<div class="carousel carousel-slider">
-				<a class="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1"/></a>
-				<a class="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2"/></a>
-				<a class="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3"/></a>
-				<a class="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4"/></a>
-			</div>
-			<a class='dropdown-trigger btn' href='#' onClick={this.drop()} data-target='dropdown1'>Drop Me!</a>
-
-			<ul id='dropdown1' class='dropdown-content'>
-				<li><a href="#!">one</a></li>
-				<li><a href="#!">two</a></li>
-				<li class="divider" tabindex="-1"></li>
-				<li><a href="#!">three</a></li>
-				<li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-				<li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
-			</ul>
 
 
 			</>
