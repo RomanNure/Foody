e2de1e44aa38722e90fcd2f903ff8bad4c7d5e83
Roman <rk@pangnote.com>
2019-06-11 00:18:24 +0300

    -update server, remove tresh

--- webapp/server/api.js
+++ webapp/server/api.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-//import { Roles } from 'meteor/alanning:roles'; 
-//import { check, Match } from 'meteor/check';
-//import { Accounts } from 'meteor/accounts-base';
+import { Roles } from 'meteor/alanning:roles'; 
+import { check, Match } from 'meteor/check';
+import { Accounts } from 'meteor/accounts-base';
 
 import { DB_Recipe } from '/imports/api/recipes.js';
 import { DB_Reviews } from '/imports/api/review.js';
@@ -22,7 +22,63 @@ if(Meteor.isServer){
 		
 		]
 	})
+	Meteor.methods({
+		'recipe.add'(data){
+			check(data,{
+				title: String,
+				RBody: String,
+				ingr: [String],
+				steps: [String],
+				tags: Match.Maybe(String),
+				imgs: Match.Maybe([String])
+			})
+			if(!Meteor.userId()) throw new Meteor.Error('please authorize to continue');
+			let userId = Meteor.users.findOne({_id:Meteor.userId()})
+			if(!userId) throw new Meteor.Error('please authorize to continue');
+			console.log('user =>',user);
+			let	user = {
+				name: userId.name,
+				img: userId.img,
+				id: userId._id,
+			}
+			data.user = user;
+			DB_Recipe.insert({data})
+		},
+		'recipe.edit'(id){
+			if(Meteor.userId()){
+				console.log(' user=>', userId);
+			}
+			let recipe = DB_Recipes.find({_id:id});
+			if(recipe) return recipe;
+			return 'no roots';
+
+		},
+		'review.add'(data){
+			check(data,{
+				title: String,
+				RBody: String,
+				mark: Number,
+				imgs: Match.Maybe([String])
+			})
+			if(!Meteor.userId()) throw new Meteor.Error('please authorize to continue');
+			let userId = Meteor.users.findOne({_id:Meteor.userId()})
+			if(!userId) throw new Meteor.Error('please authorize to continue');
+			console.log('user =>',user);
+			let	user = {
+				name: userId.name,
+				img: userId.img,
+				id: userId._id,
+			}
+			data.user = user;
+			DB_Recipe.insert({data})
+		},
 
 
+	})
 
+	Accounts.onCreateUser((options, user) => {
+		user.profile = options.profile || {};
+		if (user.email && (user.emails[0].address == "roman.kameniev@gmail.com")) user.roles = ['admin']
+		return user;
+	});
 }
